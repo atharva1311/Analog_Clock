0e5a0c854a81544a684bd0196ddc1b7f7386f2b2
atharva <atharvasjadhav459945@gmail.com>
2023-08-23 10:00:02 +0530

    FFFF

--- clock.js
+++ clock.js
@@ -1,16 +1,17 @@
 setInterval(() => {
-         d  =new Date();
-         htime=d.getHours();
-         mtime=d.getMinutes();
-         stime=d.getSeconds();
+  d = new Date();
+  htime = d.getHours();
+  mtime = d.getMinutes();
+  stime = d.getSeconds();
 
-         hrotation= 30*htime + mtime/2;
-         mrotation= 6*mtime;
-         srotation= 6*stime;
+  hrotation = 30 * htime + mtime / 2;
+  mrotation = 6 * mtime;
+  srotation = 6 * stime;
 
-        hour.style.transform = `rotate(${hrotation}deg)`;
-        minute.style.transform = `rotate(${mrotation}deg)`;
-       
-        second.style.transform = `rotate(${srotation}deg)`;
+  hour.style.transform = `rotate(${hrotation}deg)`;
+  minute.style.transform = `rotate(${mrotation}deg)`;
 
-},1000);
+  second.style.transform = `rotate(${srotation}deg)`;
+
+  // Atharva jadhav
+}, 1000);
